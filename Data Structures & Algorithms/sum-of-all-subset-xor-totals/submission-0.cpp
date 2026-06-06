66f0551804acc43b433dbacf41f777e4f48aa117
Akshat Bhatnagar <56737367+akshat12000@users.noreply.github.com>
2026-06-06 13:20:24 +0530

    Add: sum-of-all-subset-xor-totals - submission-0

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=0;i<(1<<n);i++){
+            int res = 0;
+            for(int j=0;j<n;j++){
+                if(i & (1<<j)){
+                    // cout<<i<<" "<<nums[j]<<"\n";
+                    res=res^nums[j];
+                }
+            }
+            ans+=res;
+        }
+        return ans;
+    }
+};
