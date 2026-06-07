4e8727ec12591c09d7e9e2ca873b2b69d8153144
Akshat Bhatnagar <56737367+akshat12000@users.noreply.github.com>
2026-06-07 15:28:48 +0530

    Add: n-queens-ii - submission-1

---
+++ Data Structures & Algorithms/n-queens-ii/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    vector<string> temp;
+    set<vector<string>> res;
+    bool isSafe(int r, int c, int n) {
+
+        for (int j = c - 1; j >= 0; j--)
+            if (temp[r][j] == 'Q')
+                return false;
+
+        for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--)
+            if (temp[i][j] == 'Q')
+                return false;
+
+        for (int i = r + 1, j = c - 1; i < n && j >= 0; i++, j--)
+            if (temp[i][j] == 'Q')
+                return false;
+
+        return true;
+    }
+
+    void solveRec(int col, int n) {
+        if (col == n) {
+            res.insert(temp);
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (isSafe(row, col, n)) {
+                temp[row][col] = 'Q';
+                solveRec(col + 1, n);
+                temp[row][col] = '.';
+            }
+        }
+    }
+public:
+    int totalNQueens(int n) {
+        temp = vector<string>(n, string(n, '.'));
+        solveRec(0,n);
+        return res.size();
+    }
+};
