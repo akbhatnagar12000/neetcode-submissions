e6297e8e7fc554e5eb29cb61a299dc7261a5e8eb
Akshat Bhatnagar <56737367+akshat12000@users.noreply.github.com>
2026-07-18 12:23:13 +0530

    Add: minimum-path-sum - submission-0

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<>>pq;
+        pq.push({grid[0][0],0,0});
+        while(!pq.empty()){
+            auto p = pq.top();
+            pq.pop();
+            int s = get<0>(p);
+            int x = get<1>(p);
+            int y = get<2>(p);
+            if(x==n-1 && y==m-1){
+                return s;
+            }
+            if(dp[x][y]<s){
+                continue;
+            }
+            if(x+1<n){
+                pq.push({s+grid[x+1][y],x+1,y});
+            }
+            if(y+1<m){
+                pq.push({s+grid[x][y+1],x,y+1});
+            }
+        }
+        return -1;
+    }
+};
