1611eb131ed667399f7924a2620b3a29b3abd232
akbhatnagar12000 <bhatnagarakshat174@gmail.com>
2026-07-28 17:05:32 +0530

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if(intervals.empty()){
+            return 0;
+        }
+        sort(intervals.begin(), intervals.end(), [](auto& x, auto& y) {
+            return x.start < y.start;
+        });
+        int ans = 1;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        pq.push(intervals[0].end);
+        for (int i = 1; i < intervals.size(); ++i) {
+            if(!pq.empty() && intervals[i].start>=pq.top()){
+                pq.pop();
+                pq.push(intervals[i].end);
+                continue;
+            }
+            if (intervals[i].start < intervals[i - 1].end) {
+                ans++;
+            }
+            pq.push(intervals[i].end);
+        }
+        return ans;
+    }
+};
