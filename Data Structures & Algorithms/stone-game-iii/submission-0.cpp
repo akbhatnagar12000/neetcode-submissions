684a0dd257dd5238626edf2de365090b882389a1
akbhatnagar12000 <bhatnagarakshat174@gmail.com>
2026-08-03 17:34:58 +0530

    Add: stone-game-iii - submission-0

---
+++ Data Structures & Algorithms/stone-game-iii/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    vector<int> dp;
+
+    int solve(vector<int>& stoneValue, int i) {
+        if (i >= stoneValue.size())
+            return 0;
+
+        if (dp[i] != INT_MIN)
+            return dp[i];
+
+        int sum = 0;
+        int ans = INT_MIN;
+
+        for (int k = 0; k < 3 && i + k < stoneValue.size(); k++) {
+            sum += stoneValue[i + k];
+            ans = max(ans, sum - solve(stoneValue, i + k + 1));
+        }
+
+        return dp[i] = ans;
+    }
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        dp.assign(n, INT_MIN);
+
+        int diff = solve(stoneValue, 0);
+
+        if (diff > 0) return "Alice";
+        if (diff < 0) return "Bob";
+        return "Tie";
+    }
+};
