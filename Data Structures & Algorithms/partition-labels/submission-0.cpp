504a615ee1189287232f13f4f1513c776e3f84ea
akbhatnagar12000 <bhatnagarakshat174@gmail.com>
2026-08-09 17:07:56 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.cpp
@@ -0,0 +1,53 @@
+class Solution {
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>res;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+            if(end>=intervals[i][0]){
+                end=max(end,intervals[i][1]);
+            }
+            else{
+                res.push_back({start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
+            }
+        }
+        res.push_back({start,end});
+        return res;
+    }
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int>firstOccur(26,-1),lastOccur(26,-1);
+        for(int i=0;i<s.length();i++){
+            if(firstOccur[s[i]-'a']<0){
+                firstOccur[s[i]-'a']=i;
+            }
+        }
+        for(int i=s.length()-1;i>=0;i--){
+            if(lastOccur[s[i]-'a']<0){
+                lastOccur[s[i]-'a']=i;
+            }
+        }
+        vector<vector<int>>intervals;
+        for(int i=0;i<26;i++){
+            if(firstOccur[i]>=0){
+                intervals.push_back({firstOccur[i],lastOccur[i]});
+            }
+        }
+        // for(auto x:intervals){
+        //     cout<<x[0]<<" "<<x[1]<<"\n";
+        // }
+        intervals = merge(intervals);
+        // cout<<"\nAfter\n";
+        // for(auto x:intervals){
+        //     cout<<x[0]<<" "<<x[1]<<"\n";
+        // }
+        vector<int>res;
+        for(auto x:intervals){
+            res.push_back(x[1]-x[0]+1);
+        }
+        return res;
+    }
+};
